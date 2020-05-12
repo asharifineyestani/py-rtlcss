f62c94960e3955094e731e10762668e362bee6d0
Ali-Sharifi-Neyestani <sh4rifi@gmail.com>
2020-05-13 03:10:06 +0430

    minor modify [rename to MakeRtl]

---
+++ inc/MakeRtl.py
@@ -0,0 +1,20 @@
+import os
+
+
+class MakeRtl:
+    dirs = []
+
+    def __init__(self):
+        self.target_dir = '../scss'
+        self.set_files()
+
+    def set_files(self):
+        for path, sub_dirs, files in os.walk(self.target_dir):
+            for name in files:
+                self.dirs.append(os.path.join(path, name))
+
+
+s = Rtl()
+
+for x in s.dirs:
+    print(x)
