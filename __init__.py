515dd84f5687b4a469070a994f0b9b482471ed0e
Ali-Sharifi-Neyestani <sh4rifi@gmail.com>
2020-05-15 07:49:28 +0430

    add convert_map_to_laravel_loop & work on react to html

--- __init__.py
+++ __init__.py
@@ -2,8 +2,8 @@ import re
 
 
 class MakeDiff:
-    from_path = 'css/Footer.js'
-    to_path = 'css/section.blade.php'
+    from_path = 'for_test/object.js'
+    to_path = 'for_test/array.php'
 
     def __init__(self):
         self.clear_result_file()
@@ -11,10 +11,37 @@ class MakeDiff:
     def clear_result_file(self):
         open(self.to_path, 'w').close()
 
+    def convert_map_to_laravel_loop(self):
+        from_path = open(self.from_path, "r")
+        from_lines = ''.join(from_path.readlines())
+
+        # convert Link to <a/>
+        pattern_link = r'<Link\sto=(\{*\"*.*?\"*}*)>([\s\S]*?)</Link>'
+        translate_a = r'<a href="\1">\2</a>'
+        new_string = re.sub(pattern_link, translate_a, from_lines)
+
+        # find maps
+        pattern_map = r'\{.*map.*\(([\s\S]*?)\)\)\}'
+        maps_found = re.findall(pattern_map, new_string)
+        joined_maps_found = ''.join(maps_found)
+
+        # convert js variables to php variables
+        temp_map = re.sub(r'{', '{{$', joined_maps_found)
+        temp_map = re.sub(r'\}', '}}', temp_map)
+        temp_map = re.sub(r'\.', '->', temp_map)
+
+        # remove key
+        temp_map = re.sub(r'\s*key=\{.*}', '', temp_map)
+        map_child = re.sub(pattern_map, temp_map, new_string)
+        laravel_foreach_template = "@foreach($items as $item)%@endforeach"
+        laravel_foreach_final = re.sub(r'%', map_child, laravel_foreach_template)
+        print(laravel_foreach_final)
+
     def update(self):
         ali = open(self.from_path, "r")
         s = ''.join(ali.readlines())
-        s = self.obj_to_array(s)
+        # s = self.obj_to_array(s)
+        s = self.react_to_html(s)
 
         with open(self.to_path, "a") as result:
             result.write(s)
@@ -28,9 +55,29 @@ class MakeDiff:
         s = re.sub(p, to, s)
         s = re.sub('\s*{', '\n[\n', s)
         s = re.sub('\s*}', '\n]', s)
-        return s;
+        return s
+
+    @staticmethod
+    def react_to_html(s):
+        p = r'<Link\sto=\{*\"*(.*?)\"*}*>([\s\S]*?)</Link>'
+        to = r'<a href="\1">\2</a>'
+        s = re.sub(p, to, s)
+
+        p = r'className="(.*)"'
+        to = r'class="\1"'
+        s = re.sub(p, to, s)
+
+        p = r'<Container>'
+        to = r'<div class="container">'
+        s = re.sub(p, to, s)
+
+        p = r'key=\{.*}'
+        to = r''
+        s = re.sub(p, to, s)
+
+        return s
 
 
 diff = MakeDiff()
-diff.update()
+diff.convert_map_to_laravel_loop()
 # diff.test()
